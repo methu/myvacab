3306f677547d8f75fe7fffab93033c8f3fb2790b
Jiuyang Zhang <Alan@Alans-iMac.local>
2020-12-01 19:46:14 +0900

    use react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
 
-import Navbar from "./components/navbar.component";
 import RandomPhrase from "./components/random-phrase.component";
 import PhrasesList from "./components/phrases-list.component";
 import NewPhrase from "./components/add-phrase.component";
@@ -14,7 +15,17 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <Navbar />
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">My Vocab</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/phrases">Phrases</Nav.Link>
+              <Nav.Link href="/words">Words</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
         <Route path="/" exact component={RandomPhrase} />
         <Route path="/phrases" exact component={PhrasesList} />
         <Route path="/phrases/add" excat component={NewPhrase} />
